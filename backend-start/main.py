5c042dd0d47eeae684c8616808e774f150d076f5
adeliya-isabaeva <adeliya.isabaeva@gmail.com>
2026-07-17 21:01:48 +0300

    feat: added books list and show_books function

--- backend-start/main.py
+++ backend-start/main.py
@@ -1,4 +1,18 @@
-import sys
-print("Проект запущен. День 1.")
-print("Путь к интерпретатору:", sys.executable)
-print("Проверка коммита")
+# import sys
+# print("Проект запущен. День 1.")
+# print("Путь к интерпретатору:", sys.executable)
+# print("Проверка коммита")
+books = [
+    {"title": "Война и мир", "author": "Толстой", "pages": 1225},
+    {"title": "Преступление и наказание", "author": "Достоевский", "pages": 672},
+    {"title": "Анна Каренина", "author": "Толстой", "pages": 864},
+    {"title": "Отцы и дети", "author": "Тургенев", "pages": 288},
+    {"title": "Мёртвые души", "author": "Гоголь", "pages": 352},
+]
+def show_books(book_list):
+    for book in book_list:
+        print(f"{book['title']} — {book['author']}")
+
+# Вызываем функцию
+show_books(books)
+
